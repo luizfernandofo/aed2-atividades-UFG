1351c80e4bdb4cebffe4a1c596771c8cf4dd55ae
Luiz Fernando <60102191+luizfernandofo@users.noreply.github.com>
2021-08-26 09:44:24 -0300

    comentário no código

--- uri/uri1861.cpp
+++ uri/uri1861.cpp
@@ -3,52 +3,75 @@
 
 using namespace std;
 
-class Killer{
+class Killer{ // classe que representa um assassino
 
     public:
-        string name;
-        bool alive;
-        int kills;
+        string name; // string para o nome
+        bool alive; // checar se ele foi morto ou não
+        int kills; // quantidade de pessoas que ele assassinou
 
-        Killer(){
+        Killer(){ // construtor
             alive = true;
             kills = 1;
         }
 };
 
-class TreeNode{
+class TreeNode{ // classe molde para um nó da árvore
 
     public:
-        Killer killer;
+        Killer killer; // data contida no nó 
 
-        TreeNode *LeftChild, *RightChild;
+        TreeNode *LeftChild, *RightChild; // ponteiros para nós filhos
 
     public:
 
-        TreeNode(){
+        TreeNode(){ // construtor
             LeftChild = RightChild = NULL;
         }
 };
 
-class BSTree{
+class BSTree{ // classe da árvore 
 
     public:
-        TreeNode *Root;
+        TreeNode *Root; // nó raiz
 
     public:
-    
+
+        /**
+         * @brief Função para inserir um nó na árvore de forme binária.
+         * 
+         * @param killer ponteiro de uma string para o nome do assassino
+         */
         void InsertNode(string *killer);
 
+        /**
+         * @brief Função que verifica se um assassino contido na árvore foi morto. Caso tenha sido a variável alive (bool) recebe false.
+         * 
+         * @param killer ponteiro para a string com o nome da pessoa assassinada.
+         * @return int retorna 1 se o nome enviado é alguém que está na árvore e 0 caso não.
+         */
         int Killed(string *killer);
 
+        /**
+         * @brief Função que percorre a árvore de forma InOrder e desaloca o ponteiro root no final.
+         * 
+         * @param root ponteiro nó onde será iniciado o percurso.
+         */
         void InOrder(TreeNode *root);
 
-        BSTree(){
+        BSTree(){ // construtor
             Root = NULL;
         }
 
 };
 
+/**
+ * @brief Função para comparar 2 strings.
+ * 
+ * @param str1 ponteiro para a string 1.
+ * @param str2 ponteiro para a string 2.
+ * @return int retorna -1 se str1 preceder str2; 0 se forem iguais; 1 se str2 preceder str1; 3 se ocorrer algum erro.
+ */
 int compare(string *str1, string *str2);
 
 int main(void)
@@ -62,18 +85,18 @@ int main(void)
     string temp_name;
 
     while(1){
-        cin >> temp_name;
-        if(cin.eof()) break;
-        killers_tree.InsertNode(&temp_name);
-        cin >> temp_name;
-        killed.push_back(temp_name);
+        cin >> temp_name; // lê o primeiro nome
+        if(cin.eof()) break; // checa se há fim do arquivo logo
+        killers_tree.InsertNode(&temp_name); // insere um nó com o nome lido
+        cin >> temp_name; // lê um segundo nome
+        killed.push_back(temp_name); // insere o nome dos assassinados em um vetor para futura consulta na árvore
     }
     
-    for(i=0; i<killed.size(); i++) killers_tree.Killed(&killed[i]);
+    for(i=0; i<killed.size(); i++) killers_tree.Killed(&killed[i]); // chama a função BSTree::Killed()
     
-    cout << "HALL OF MURDERERS" << endl;
+    cout << "HALL OF MURDERERS" << endl; 
 
-    killers_tree.InOrder(killers_tree.Root);
+    killers_tree.InOrder(killers_tree.Root); // chama a função de percurso iniciando no nó da árvore
 
     return 0;
 }
@@ -83,7 +106,7 @@ void BSTree::InsertNode(string *killer){
 
     TreeNode *Tmp = NULL, *NewNode=NULL;
 
-        if( Root == NULL) {
+        if( Root == NULL) {  
             NewNode = new TreeNode;
             NewNode->killer.name = *killer;
             Root = NewNode;
